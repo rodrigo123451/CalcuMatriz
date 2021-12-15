a710af3555792f1cb45f525700d8d9a1f0d7ef90
rodrigo123451 <85976210+rodrigo123451@users.noreply.github.com>
2021-12-15 00:28:26 -0400

    Add files via upload

---
+++ geometrica/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class GeometricaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'geometrica'
+    varbose_name = 'Geometrica'
